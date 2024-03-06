ed07b0c09af4e35db82e0166d3dc20bd26b8218f
ZsoltSzilagyi1 <z_szilgyi@yahoo.com>
2024-03-06 12:25:38 +0200

    curs6 sass/less

---
+++ curs6/js/ap1_1.js
@@ -0,0 +1,17 @@
+function add(x,y){
+    let s = x + y;
+    return s;
+}
+
+function diff(x,y){
+    let d = x - y;
+    return d;
+}
+
+function prod(x,y){
+    let p = x * y;
+    return p;
+}
+
+export{add, diff, prod};
+
---
+++ curs6/js/ap1_2.js
@@ -0,0 +1,5 @@
+import{add, prod, diff} from "./ap1_1.js";
+
+console.log(add(1,2));
+console.log(prod(3,4));
+console.log(diff(5,2));
